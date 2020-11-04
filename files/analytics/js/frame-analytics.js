f3704eac591e981a51564ab4e4936f22ad69c931
q0l <73968120+q0l@users.noreply.github.com>
2020-11-05 01:00:32 +0300

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -1,7 +1,1 @@
-window.fileIntegrity = window.fileIntegrity;
-					// securely add the analytics code into iframe
-				script = document.createElement('script');
-				script.setAttribute('src', 'xxx');
-				script.setAttribute('integrity', 'sha256-'+fileIntegrity.value);
-				script.setAttribute('crossorigin', 'anonymous');
-				analyticsFrame.contentDocument.body.appendChild(script);
+alert(6)
