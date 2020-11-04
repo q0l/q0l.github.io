ae6c6753c87a4b200e4d69534deb2eaeca69835e
q0l <73968120+q0l@users.noreply.github.com>
2020-11-05 01:12:01 +0300

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -3,3 +3,4 @@ console.log('User Agent String: ' + navigator.userAgent);
 console.log('User Agent Vendor: ' + navigator.vendor);
 console.log('User OS: ' + navigator.platform);
 console.log('User Language: ' + navigator.language);
+window.name='dsfdsf';
