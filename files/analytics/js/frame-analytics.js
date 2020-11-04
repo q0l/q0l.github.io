0e75ff325a74a9504802913788bfbcdc4b5aaa00
q0l <73968120+q0l@users.noreply.github.com>
2020-11-05 00:53:15 +0300

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -1,14 +1,5 @@
 window.fileIntegrity = window.fileIntegrity;
-	
-
-				
-				// create a sandboxed iframe
-				analyticsFrame = document.createElement('iframe');
-				analyticsFrame.setAttribute('sandbox', 'allow-scripts allow-same-origin');
-				analyticsFrame.setAttribute('class', 'invisible');
-				document.body.appendChild(analyticsFrame);
-
-				// securely add the analytics code into iframe
+					// securely add the analytics code into iframe
 				script = document.createElement('script');
 				script.setAttribute('src', 'xxx');
 				script.setAttribute('integrity', 'sha256-'+fileIntegrity.value);
