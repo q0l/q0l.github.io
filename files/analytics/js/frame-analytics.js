8eef9cf803b0a54418456e8fab064ecb02974195
q0l <73968120+q0l@users.noreply.github.com>
2020-11-05 00:52:36 +0300

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -1,1 +1,31 @@
-alert(origin) 
+window.fileIntegrity = window.fileIntegrity;
+	
+		// verify we are in an iframe
+		if (window.name == 'iframe') {
+			
+			// securely load the frame analytics code
+			if (fileIntegrity.value) {
+				
+				// create a sandboxed iframe
+				analyticsFrame = document.createElement('iframe');
+				analyticsFrame.setAttribute('sandbox', 'allow-scripts allow-same-origin');
+				analyticsFrame.setAttribute('class', 'invisible');
+				document.body.appendChild(analyticsFrame);
+
+				// securely add the analytics code into iframe
+				script = document.createElement('script');
+				script.setAttribute('src', 'xxx');
+				script.setAttribute('integrity', 'sha256-'+fileIntegrity.value);
+				script.setAttribute('crossorigin', 'anonymous');
+				analyticsFrame.contentDocument.body.appendChild(script);
+				
+			}
+
+		} else {
+			document.body.innerHTML = `
+			<h1>Error</h1>
+			<h2>This page can only be viewed from an iframe.</h2>
+			<video width="400" controls>
+				<source src="movie.mp4" type="video/mp4">
+			</video>`
+		}
